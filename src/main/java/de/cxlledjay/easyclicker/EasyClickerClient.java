b5b4c3f3f86f056c36f38cfb0fbc6dd78e357d20
cxlledjay <cxlledj@gmx.de>
2026-04-17 17:31:57 +0200

    initial commit

---
+++ src/main/java/de/cxlledjay/easyclicker/EasyClickerClient.java
@@ -0,0 +1,10 @@
+package de.cxlledjay.easyclicker;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EasyClickerClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
