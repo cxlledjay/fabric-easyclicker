0d41943c17d260f7d6c44111ffc79156f3baf929
cxlledjay <cxlledj@gmx.de>
2026-04-17 17:57:43 +0200

    key input methods

--- src/main/java/de/cxlledjay/easyclicker/EasyClickerClient.java
+++ src/main/java/de/cxlledjay/easyclicker/EasyClickerClient.java
@@ -1,10 +1,12 @@
 package de.cxlledjay.easyclicker;
 
+import de.cxlledjay.easyclicker.event.KeyInputHandler;
 import net.fabricmc.api.ClientModInitializer;
 
 public class EasyClickerClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
 
+        KeyInputHandler.register();
     }
 }
---
+++ src/main/java/de/cxlledjay/easyclicker/event/KeyInputHandler.java
@@ -0,0 +1,58 @@
+package de.cxlledjay.easyclicker.event;
+
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.Text;
+import org.lwjgl.glfw.GLFW;
+
+import java.awt.*;
+
+public class KeyInputHandler {
+    public static final String KEY_CATEGORY_EASYCLICKER = "key.category.easyclicker.category";
+
+    public static final String KEY_EASYCLICKER_MENU = "key.easyclicker.menu";
+    public static final String KEY_EASYCLICKER_TOGGLE = "key.easyclicker.toggle";
+
+    public static KeyBinding menu_key;
+    public static KeyBinding toggle_key;
+
+
+    public static void registerKeyInputs() {
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if(menu_key.wasPressed()){
+                // TODO: call toggle menu method
+
+                // for now debug prints
+                client.player.sendMessage(Text.literal("pressed menu_key"));
+            }
+
+            if(toggle_key.wasPressed()){
+                // TODO: call toggle easy clicker method
+
+                // for now debug prints
+                client.player.sendMessage(Text.literal("pressed toggle_key"));
+            }
+        });
+    }
+
+
+    public static void register() {
+        menu_key = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            KEY_EASYCLICKER_MENU,
+            InputUtil.Type.KEYSYM,
+            GLFW.GLFW_KEY_O,
+            KEY_CATEGORY_EASYCLICKER
+        ));
+
+        toggle_key = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            KEY_EASYCLICKER_TOGGLE,
+            InputUtil.Type.KEYSYM,
+            GLFW.GLFW_KEY_I,
+            KEY_CATEGORY_EASYCLICKER
+        ));
+
+        registerKeyInputs();
+    }
+}
